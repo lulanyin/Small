17ff755dd77e6c497196a0834fcfab1e881c4b5e
Hunter <lulanyin@qq.com>
2018-12-20 00:49:32 +0800

    增加文件上传处理控制器

---
+++ src/http/files/uploadController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Small\http\files;
+
+use Small\http\HttpController;
+
+class uploadController extends HttpController{
+
+    public function index(...$args)
+    {
+        // TODO: Implement index() method.
+    }
+}
